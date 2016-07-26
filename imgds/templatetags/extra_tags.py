1fcc3eea5c36d94b1a53e84fa153f48fa1b114f1
Edward Betts <edward@4angle.com>
2016-07-26 18:23:21 +0100

    Optimize the code.
    I made the code shorter, it does the same thing.

--- imgds/templatetags/extra_tags.py
+++ imgds/templatetags/extra_tags.py
@@ -2,15 +2,10 @@ from django import template
 register = template.Library()
 
 @register.filter
-def hash(h,key):
-    if key in h.keys():
-        return h[key]
-    else:
-        return None
+def hash(h, key):
+    return h.get(key)
 
 @register.filter
-def image(h,key):
-    if key in h.keys():
+def image(h, key):
+    if key in h:
         return h[key].image
-    else:
-        return None
